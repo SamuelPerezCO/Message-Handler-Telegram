3fe32d17f1cb10385274dde565d31b20cb591976
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-04-29 07:52:11 -0500

    everything

--- main.py
+++ main.py
@@ -9,16 +9,40 @@ load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-
-async def say_hello (update:Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Conversacion Iniciada")
+GET_NAME , GET_LAST_NAME = range(2)
 
 
-async def send_message (update:Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Has escrito un mensaje")
+async def ask_for_name (update:Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("What's your name")
+    return GET_NAME
+
+
+async def get_name (update:Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["name"] = update.message.text
+    await update.message.reply_text("What's your last name")
+    return GET_LAST_NAME
+
+
+async def get_last_name (update:Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["last_name"] = update.message.text
+    await update.message.reply_text(
+        f"""Thank You , The data is Name : {context.user_data['name']} , last name: {context.user_data['last_name']}""")
+    return ConversationHandler.END
+
+async def cancel_conversation (update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Cancel Conversation")
+
+
+info_conversation_handler = ConversationHandler(
+    entry_points=[ CommandHandler("data" , ask_for_name)],
+    states={
+        GET_NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_name)],
+        GET_LAST_NAME:[MessageHandler(filters.TEXT & ~filters.COMMAND, get_last_name)],
+    },
+    fallbacks=[MessageHandler(filters.COMMAND , cancel_conversation)]
+)
 
 application = ApplicationBuilder().token(TOKEN).build()
-application.add_handler(CommandHandler("start", say_hello) )
-application.add_handler( MessageHandler(filters.TEXT & ~filters.AUDIO , send_message ))
+application.add_handler( info_conversation_handler)
 
 application.run_polling(allowed_updates=Update.ALL_TYPES)
